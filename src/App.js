3512e7a501cc8d2fff2379af14610944b9807859
Yassir Musarrat <yassir.musarrat@gmail.com>
2023-11-09 20:30:42 +0400

    Reacttask1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WordCounter from './WordC.js';
+
+function App() {
+  return (
+    <div className="App">
+      <WordCounter />
+      
+    </div>
+  );
+}
+
+export default App;
